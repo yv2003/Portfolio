50d78f8145bd5184cda87205377ba5bf55e52daa
Yashasvi Vaidya <yashasvivaidya13@gmail.com>
2025-01-17 20:15:45 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -141,7 +141,7 @@ export default function Page() {
                   key={project.title}
                   title={project.title}
                   description={project.description}
-                  dates={project.dates}
+                  // dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
                   // video={project.video}
